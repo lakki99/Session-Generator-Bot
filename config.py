0cc8d4b044fe7e4904385cee0d4f583819b2653d
Lakki reddy <pulka143341@gmail.com>
2025-07-16 18:45:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "7460363208")) # Owner Id or Admin Id
 F_SUB = environ.get("F_SUB", "")
 
 # Mongodb Database Uri For User Data Store 
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://eno2223456:7Cdmqig5Ih2vrqW4@cluster0.ccpmee5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
 
 # Port To Run Web Application 
 PORT = int(environ.get('PORT', 8080))
